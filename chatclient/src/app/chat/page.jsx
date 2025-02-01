09cfe54f09e4a1624c89c787778146ae99c4aaa0
Niroj925 <ngtc0925@gmail.com>
2025-02-01 13:07:26 +0545

    implement rabbitMQ

--- chatclient/src/app/chat/page.jsx
+++ chatclient/src/app/chat/page.jsx
@@ -1,124 +1,134 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import styles from './chat.module.css';
-import { useSearchParams } from 'next/navigation';
-import api from '../component/api/api';
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+import styles from "./chat.module.css";
+import { useSearchParams } from "next/navigation";
+import api from "../component/api/api";
 
-
-const socket = io('http://localhost:4040/message');
+const socket = io("http://localhost:4040/message");
 
 const ChatComponent = () => {
-    const [messages, setMessages] = useState([]);
-    const [inputMessage, setInputMessage] = useState('');
-    const [room, setRoom] = useState('');
-    const [users, setUsers] = useState([]);
-    const [isRoomExist,setIsRoomExist]=useState(false);
-   
-    const params = useSearchParams();
-    const user1 = params.get('id');
-    console.log(user1);
+  const [messages, setMessages] = useState([]); 
+  const [inputMessage, setInputMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState([]);
+  const [isRoomExist, setIsRoomExist] = useState(false);
+  const [activeUser, setActiveUser] = useState("");
+  const activeUserRef = useRef(activeUser);
 
-    const fetchAllUser = async () => {
-        try {
-            const response = await api.get('/user');
-            console.log(response);
-            if (response.status == 200) {
-                setUsers(response.data);
-            }
-        } catch (err) {
-            console.log(err);
-        }
+  const params = useSearchParams();
+  const user1 = params.get("id");
+
+  console.log('au:',activeUser)
+
+  const fetchAllUser = async () => {
+    try {
+      const response = await api.get("/user");
+      if (response.status === 200) {
+        setUsers(response.data);
+      }
+    } catch (err) {
+      console.error("Error fetching users:", err);
     }
+  };
 
-    useEffect(() => {
-        fetchAllUser();
-    }, []);
+  useEffect(() => {
+    activeUserRef.current = activeUser;
+  }, [activeUser]); 
+  
 
-    useEffect(() => {
-        socket.on('message', (message) => {
-          console.log(message);
-            setMessages(message);
-        });
-        socket.on('receiveMessage', (message) => {
-            console.log(message);
-              setMessages(message);
-          });
-    }, []);
+  useEffect(() => {
+    fetchAllUser();
+  }, []);
 
-
-    const joinFrn = async (user2) => {
-        setMessages([]);
-        try{
-            const response = await api.get(`/user/frn?user1=${user1}&user2=${user2}`);
-            console.log(response);
-            if(response.status===200){
-                const room=response.data.id;
-                setRoom(room);
-                setIsRoomExist(true);
-                console.log('room:',room);
-                socket.emit('join', room); 
-
-            }
-        }catch(err){
-            console.log('user not found');
+  useEffect(() => {
+    const messageListener = (message) => {
+        if (message.sender === activeUserRef.current) {
+          setMessages((prev) => [...prev, message.message]);  
         }
-        
-    }
+      };
 
-    const handleSend = () => {
-        if (inputMessage.trim() !== '') {
-            const message = { sender:user1,message:inputMessage,room};
-            socket.emit('createMessage', message);
-            setInputMessage('');
-        }
+    const receiveMessageListener = (message) => {
+      let msgList = message.map((msg) => msg.message);
+      setMessages(msgList);  
     };
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.userBox}>
-            {
-                    users && users.map((usr, index) => {
-                        return (
-                            <>
-                                {
-                                    user1 !== usr.id && (
-                                        <div key={index} className={styles.user} onClick={() => { joinFrn(usr.id) }}>
-                                            <p>{usr.email}</p>
-                                        </div>
-                                    )
-                                }
-                            </>
+    socket.on("message", messageListener);
+    socket.on("receiveMessage", receiveMessageListener);
 
-                        )
-                    })
-                }
+    return () => {
+      socket.off("message", messageListener);
+      socket.off("receiveMessage", receiveMessageListener);
+    };
+  }, []);
+
+  const joinFrn = async (user2) => {
+    setActiveUser(user2);
+    setMessages([]);  
+    try {
+      const response = await api.get(`/user/frn?user1=${user1}&user2=${user2}`);
+      if (response.status === 200) {
+        const room = response.data.id;
+        setRoom(room);
+        setIsRoomExist(true);
+        socket.emit("join", room);
+      }
+    } catch (err) {
+      setRoom(null);
+      console.error("User not found:", err);
+    }
+  };
+
+  const handleSend = () => {
+    if (inputMessage.trim() !== "") {
+      const message = { sender: user1, message: inputMessage, room };
+      socket.emit("createMessage", message);
+      setMessages((prev) => [...prev, inputMessage]);  
+      setInputMessage("");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.userBox}>
+        {users &&
+          users.map((usr, index) => (
+            user1 !== usr.id && (
+              <div
+                key={index}
+                className={styles.user}
+                style={{ backgroundColor: usr.id === activeUser ? "#dad4d4" : "#9eafb9" }}
+                onClick={() => joinFrn(usr.id)}
+              >
+                <p>{usr.email}</p>
+                <p style={{fontSize:'10px'}}>{usr.id}</p>
+              </div>
+            )
+          ))}
+      </div>
+      <div className={styles.messageBox}>
+        {isRoomExist && (
+          <div className={styles.messageContainer}>
+            <div className={styles.messsage}>
+              {messages.map((msg, index) => (
+                <p key={index}>{msg}</p>
+              ))}
             </div>
-            <div className={styles.messageBox}>
-          {
-            isRoomExist&&(
-                <div className={styles.messageContainer}>
-              <div className={styles.messsage}>
-                {messages.map((msg, index) => (
-                    <p key={index}>{msg.message}</p>
-                ))}
-            </div>
-             <div className={styles.inputMsg}>
-                <input
+            <div className={styles.inputMsg}>
+              <input
                 type="text"
                 value={inputMessage}
                 onChange={(e) => setInputMessage(e.target.value)}
                 placeholder="Type your message..."
-            />
-            <button onClick={handleSend}>Send</button>
-              </div>
-              </div>
-            )
-          }
-        </div>
-        </div>
-    );
+              />
+              <button onClick={handleSend}>Send</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ChatComponent;
